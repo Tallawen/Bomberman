65a91769296db99348e664ee091b2a5c06f83041
P4 <pawel.2706@gmail.com>
2013-03-05 22:15:15 +0100

    new Release, clean up resources
    update Release directory
    remove unused resources from Release/Data
    remove 'Data' in main project directory

---
+++ Release/Data/constants.lua
@@ -0,0 +1,47 @@
+video = 
+{
+    screen_width = 750,
+    screen_height = 700,
+}
+
+player = 
+{
+    health = 5,
+    speed = 100,
+    max_health = 5,
+    blink_time = 1,
+    immortal_time = 8 -- blink_time * 2 * x (x - ile razy)
+}
+
+bomb =
+{
+    number = 2,
+    life_time = 2
+}
+
+explosion = 
+{
+    length = 2,
+    delay = 0.1
+}
+
+ui = 
+{
+    avatar_x = 17,
+    avatar_y = 9,
+    
+    scores_x = 88,
+    scores_y = 57,
+
+    healthBar_x = 88,
+    healthBar_y = 29,
+    healthBar_lenght = 180,
+    healthBar_alpha = 70,
+    
+    bombBar_x = 88,
+    bombBar_y = 56,
+    bombBar_offset = 8,
+    
+    fps_x = 400,
+    fps_y = 30
+}
